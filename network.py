331efbd70b395902a6387c07758c4dd16e8e75e9
Adam Ulrich <a_ulrich@utb.cz>
2021-12-07 15:13:19 +0100

    transpose fixes

--- network.py
+++ network.py
@@ -18,27 +18,25 @@ class NN:
         """
         self.input = i
         self.y = y
-
         self.output = np.zeros(self.y.shape)
         # weights default 1
-        self.w1 = np.ones((len(i[0]), nhl)).T  # 4x3 matrix
-
+        self.w1 = np.ones((len(i[0]), nhl))  # 3x4 matrix
         self.w2 = np.ones((nhl, 1))  # 4x1 matrix
 
     def feedforward(self):
-        self.layer1 = sigmoid(self.input @ self.w1.T)  # 4x4 matrix
+        self.layer1 = sigmoid(self.input @ self.w1)  # 4x4 matrix
         self.output = sigmoid(self.layer1 @ self.w2)  # 4x1 matrix
 
     def backprop(self):
         derivative1 = sigmoid_derivative(self.output)
-
         error = 2 * (self.y - self.output)
-        d_weights2 = self.layer1.T @ (error * derivative1)
-
         derivative2 = sigmoid_derivative(self.layer1)
 
+        d_weights2 = self.layer1.T @ (error * derivative1)  # 4x1 matrix
+
         d_weights1 = self.input.T @ (error * derivative1 @ self.w2.T * derivative2)
-        self.w1 += d_weights1.T
+
+        self.w1 += d_weights1
         self.w2 += d_weights2
 
 
@@ -48,7 +46,7 @@ if __name__ == '__main__':
     results = np.array([[0], [1], [1], [0]])
 
     nn = NN(inputs, nhl=4, y=results)
-    for _ in range(1000000):
+    for _ in range(10000):
         nn.feedforward()
         nn.backprop()
 
